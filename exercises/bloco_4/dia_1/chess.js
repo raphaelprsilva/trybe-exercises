1e78e3275c09f755d187ec9f020ffeaf575a7912
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-03-24 19:22:01 -0300

    Adiciona codigo para verificar qual tipo de movimento faz cada peça

---
+++ exercises/bloco_4/dia_1/chess.js
@@ -0,0 +1,18 @@
+let peca = 'RAInh';
+let novaPeca = peca.toLowerCase();
+
+if (novaPeca === 'rei') {
+  console.log('O', novaPeca, 'se movimenta na horizontal, vertical e diagonal');
+} else if (novaPeca === 'rainha') {
+  console.log('O', novaPeca, 'se movimenta na horizontal, vertical e diagonal');
+} else if (novaPeca === 'bispo') {
+  console.log('O', novaPeca, 'se movimenta na diagonal');
+} else if (novaPeca === 'cavalo') {
+  console.log('O', novaPeca, 'se movimenta em L');
+} else if (novaPeca === 'torre') {
+  console.log('O', novaPeca, 'se movimenta na vertical e horizontal');
+} else if (novaPeca === 'peão') {
+  console.log('O', novaPeca, 'se movimenta apenas para frente');
+} else {
+  console.log(novaPeca, 'não é uma peça do xadres.');
+}
