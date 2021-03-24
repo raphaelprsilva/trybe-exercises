00b8fab773e643142e8c24495ab4bc06171f01c1
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-03-24 15:17:23 -0300

    Exemplo - Teste com a ferramenta switch e case

---
+++ exercises/bloco_4/dia_1/scripts_ex1_pt2.js
@@ -0,0 +1,15 @@
+let notaCandidato = "Listaa";
+
+switch (notaCandidato) {
+  case "Aprovada":
+    console.log("Aprovado!");
+    break;
+  case "Lista":
+    console.log("Lista de espera.");
+    break;
+  case "Reprovada":
+    console.log("Reprovado!");
+    break;
+  default:
+    console.log("Valor não identificado.");
+}
