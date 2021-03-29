8e114c5d20fe5518cbe3c83888c0845fa1266276
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-03-29 12:57:31 -0300

    Adiciona codigo que mostra os indices do objeto info no console

---
+++ exercises/bloco_4/dia_4/exercicio-03.js
@@ -0,0 +1,10 @@
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal do Pato Donald',
+  recorrente: 'Sim'
+};
+
+for (let index in info) {
+  console.log(index);
+}
