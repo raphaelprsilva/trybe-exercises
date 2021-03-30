89c46159ab0d43a6031c2d08d9374fe894669d1f
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-03-30 08:08:13 -0300

    Adiciona codigo que mostra o indice do array que possui o maior valor

---
+++ exercises/bloco_4/dia_4/exercicio-funcoes-02.js
@@ -0,0 +1,15 @@
+function biggestNumber(testArray) {
+  let arrayLength = testArray.length;
+  let biggestValue = 0;
+  let biggestIndex = 0;
+
+  for(let i = 0; i < arrayLength; i+=1) {
+    if (testArray[i] > biggestValue) {
+      biggestValue = testArray[i];
+      biggestIndex = i;
+    }
+  }
+  return biggestIndex;
+}
+
+console.log(biggestNumber([200, 3000, 5, 10000, 10]));
