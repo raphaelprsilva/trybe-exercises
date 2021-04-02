eeb2c6b1b2373e1e1823d02a646d9a3d9d103af5
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-04-02 09:48:05 -0300

    Finish requirement 2

--- exercises/bloco_5/dia_1/exercise-DOM-01.js
+++ exercises/bloco_5/dia_1/exercise-DOM-01.js
@@ -1,3 +1,6 @@
 document.getElementsByClassName('title')[0].innerText = "Exercício 5.1 - JavaScript";
 
 document.getElementsByTagName('p')[1].innerText = "Daqui a dois anos, eu desejo ser um dev";
+
+let elements = document.getElementsByClassName('main-content');
+elements[0].style.backgroundColor = "rgb(76,164,109)";
