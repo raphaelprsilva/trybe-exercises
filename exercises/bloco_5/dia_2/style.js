84e5fef8f36be9fbde49258823bbe77ba3347e04
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-04-05 15:57:51 -0300

    Add div uusing appendChild

--- exercises/bloco_5/dia_2/style.js
+++ exercises/bloco_5/dia_2/style.js
@@ -4,6 +4,13 @@ let addTitle = document.querySelector('.main');
 let createTitle = document.createElement('h1');
 createTitle.innerText = 'Exercicio 5.2 - JavaScript';
 
-console.log(createTitle);
 
 addTitle.appendChild(createTitle);
+
+// Adicione a tag div com a classe main-content como filho da tag body
+let addDiv = document.querySelector('.main');
+
+let createDiv = document.createElement('div');
+createDiv.className = 'main-content';
+
+addTitle.appendChild(createDiv);
