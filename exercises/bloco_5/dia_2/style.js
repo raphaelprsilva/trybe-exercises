0f166ffba7e3d77d48beb7e467adfb72ce8846d0
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-04-05 15:41:20 -0300

    Add title using appendChild

--- exercises/bloco_5/dia_2/style.js
+++ exercises/bloco_5/dia_2/style.js
@@ -1,1 +1,9 @@
 // Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body
+let addTitle = document.querySelector('.main');
+
+let createTitle = document.createElement('h1');
+createTitle.innerText = 'Exercicio 5.2 - JavaScript';
+
+console.log(createTitle);
+
+addTitle.appendChild(createTitle);
