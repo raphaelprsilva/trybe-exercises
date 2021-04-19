f1e5f273bd6d8811daebae4eda4e93c288ad777d
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-04-19 15:38:18 -0300

    Add date validation

--- exercises/bloco_6/script.js
+++ exercises/bloco_6/script.js
@@ -1,4 +1,7 @@
 const estados = document.querySelector('#estados');
+const dateElement = document.querySelector('#date');
+const submitButton = document.querySelector('#botao-enviar');
+const form = document.querySelector('form');
 
 const arrayEstados = ['', 'Acre - AC', 'Alagoas - AL', 'Amapá - AP', 'Amazonas - AM', 'Bahia - BA',
   'Ceará - CE', 'Distrito - Federal - DF', 'Espírito Santo - ES', 'Goiás - GO', 'Maranhão - MA', 'Mato Grosso - MT', 
@@ -16,3 +19,37 @@ function addEstados() {
 }
 
 addEstados();
+
+// Using reference from this site: https://www.the-art-of-web.com/javascript/validate-date/
+function checkDate() {
+  const re = /^(\d{1,2})\/(\d{1,2})\/(\d{4})$/;
+  const regs = form.elements[13].value.match(re);
+  console.log(regs);
+  console.log(form.elements[13].value);
+
+  if (form.elements[13].value !== '') {
+    if (regs !== null) {
+      if (regs[1] < 1 || regs[1] > 31) {
+        alert('O valor do dia está digitado incorretamente: ' + regs[1]);
+        return false;
+      }
+      if (regs[2] < 0 || regs[2] > 12) {
+        alert('O valor do mês está digitado incorretamente: ' + regs[2]);
+        return false;
+      }
+      if (regs[3] < 1902 || regs[3] > (new Date()).getFullYear()) {
+        alert('O valor do ano está digitado incorretamente: ' + regs[3]);
+        return false;
+      }
+    } else {
+      alert('Formato de data inválido!');
+      return false;
+    }
+  }
+
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  checkDate();
+});
