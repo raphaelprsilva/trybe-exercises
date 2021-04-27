81205e43353af7ed3bc8348c6e6e3902c2801c4b
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-04-27 14:32:50 -0300

    Add function to learn about Object.assign

---
+++ exercises/bloco_7/dia_2/example_04.js
@@ -0,0 +1,22 @@
+const student1 = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkills: 'Ótimo',
+};
+
+const student2 = {
+  Html: 'Bom',
+  Css: 'Ótimo',
+  JavaScript: 'Ruim',
+  SoftSkills: 'Ótimo',
+  Git: 'Bom', // chave adicionada
+};
+
+const objectAssign = (std1, std2) => {
+  return console.log(Object.assign(std1, std2));
+};
+
+objectAssign(student1, student2);
+
+console.log(student1);
