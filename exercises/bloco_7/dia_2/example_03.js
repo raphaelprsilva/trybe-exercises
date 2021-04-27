2561636d1cc585e86f688a9c2ae68546118ab52f
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-04-27 14:22:15 -0300

    Add function to learn about Object.entries

---
+++ exercises/bloco_7/dia_2/example_03.js
@@ -0,0 +1,20 @@
+const student1 = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkills: 'Ótimo',
+};
+
+const student2 = {
+  Html: 'Bom',
+  Css: 'Ótimo',
+  JavaScript: 'Ruim',
+  SoftSkills: 'Ótimo',
+  Git: 'Bom', // chave adicionada
+};
+
+const objectEntries = (student) => {
+  return console.log(Object.entries(student));
+};
+
+objectEntries(student1);
