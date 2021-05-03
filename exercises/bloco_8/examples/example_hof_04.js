fcb18acbc8b5bacdb15b12b4d70b147204af6e22
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-05-03 09:34:49 -0300

    Remove files from folder

--- exercises/bloco_8/examples/example_hof_04.js
+++
@@ -1,9 +0,0 @@
-const getMultiplier = (multiplier) => {
-  return (aNumber) => aNumber * multiplier;
-};
-
-const double = getMultiplier(2);
-const triple = getMultiplier(3);
-const quadruple = getMultiplier(4);
-
-console.log(double);
