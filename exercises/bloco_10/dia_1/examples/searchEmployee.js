8fd078559ef8bf1f80d217fd2eab5735cc9dca2f
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-05-19 18:12:44 -0300

    Add functions to search an employee by id and his respective test

---
+++ exercises/bloco_10/dia_1/examples/searchEmployee.js
@@ -0,0 +1,60 @@
+// Dados
+const professionalBoard = [
+  {
+    id: '8579-6',
+    firstName: 'Ana',
+    lastName: 'Gates',
+    specialities: ['UX', 'Design'],
+  },
+  {
+    id: '5569-4',
+    firstName: 'George',
+    lastName: 'Jobs',
+    specialities: ['Frontend', 'Redux', 'React', 'CSS'],
+  },
+  {
+    id: '4456-4',
+    firstName: 'Leila',
+    lastName: 'Zuckerberg',
+    specialities: ['Context API', 'RTL', 'Bootstrap'],
+  },
+  {
+    id: '1256-4',
+    firstName: 'Linda',
+    lastName: 'Bezos',
+    specialities: ['Hooks', 'Context API', 'Tailwind CSS'],
+  },
+  {
+    id: '9852-2-2',
+    firstName: 'Jeff',
+    lastName: 'Cook',
+    specialities: ['Ruby', 'SQL'],
+  },
+  {
+    id: '4678-2',
+    firstName: 'Paul',
+    lastName: 'Dodds',
+    specialities: ['Backend'],
+  },
+];
+
+const searchEmployee = (idEmployee) => {
+  if (idEmployee === undefined) {
+    throw new Error('Digite um id correto!');
+  }
+
+  const filterPerson = professionalBoard.reduce((acc, person) => {
+    if (acc !== null) return acc;
+    if (person.id === idEmployee) return person;
+    return null;
+  }, null);
+  return filterPerson;
+};
+
+// const id = '9852-2-2';
+
+// console.log(searchEmployee());
+
+module.exports = {
+  searchEmployee,
+};
