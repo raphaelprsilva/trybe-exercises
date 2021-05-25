54f112f4eef4a33967d5773e436ff66a63857753
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-05-24 21:34:06 -0300

    Add my first app

---
+++ exercises/bloco_11/dia_1/examples/testando-meu-computador/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello my dear friends.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
