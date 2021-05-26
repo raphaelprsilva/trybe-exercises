66e76c9fc42550c6b32fd3630c2bf373848f3762
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-05-26 09:01:10 -0300

    Add div do album card

--- exercises/bloco_11/dia_2/my-app/src/components/Album/Album.jsx
+++ exercises/bloco_11/dia_2/my-app/src/components/Album/Album.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import Image from '../Image';
+import './style.css';
 
 class Album extends React.Component {
   render() {
     return (
-      <section>
-        <Image
-          source={ this.props.album.image }
-          alternativeText= { this.props.album.title }
-        />
-        {/* <img src={ this.props.album.image } alt={ this.props.album.title } /> */}
-        <h2>{ this.props.album.title }</h2>
-        <p>Lançamento: { this.props.album.releaseDate.year }</p>
-        <p>Gravadora: { this.props.album.others.recordCompany }</p>
-        <p>Formatos: { this.props.album.others.formats }</p>
+      <section className="album-item">
+        <div className="album-item-card">
+          <h2>{ this.props.album.title }</h2>
+          <Image
+            source={ this.props.album.image }
+            alternativeText= { this.props.album.title }
+          />
+          {/* <img src={ this.props.album.image } alt={ this.props.album.title } /> */}
+          <p>Lançamento: { this.props.album.releaseDate.year }</p>
+          <p>Gravadora: { this.props.album.others.recordCompany }</p>
+          <p>Formatos: { this.props.album.others.formats }</p>
+        </div>
       </section>
     );
   }
