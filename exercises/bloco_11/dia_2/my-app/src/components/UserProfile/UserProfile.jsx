291f22dcce7d2c4e23c0e34a77117520a728da81
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-05-26 08:40:38 -0300

    Add div to card

--- exercises/bloco_11/dia_2/my-app/src/components/UserProfile/UserProfile.jsx
+++ exercises/bloco_11/dia_2/my-app/src/components/UserProfile/UserProfile.jsx
@@ -6,14 +6,16 @@ class UserProfile extends React.Component {
   render() {
     return (
       <div className="user-profile">
-        <p>Name: { this.props.user.name }</p>
-        <p>E-mail: { this.props.user.email }</p>
-        <Image
-          source= { this.props.user.avatar }
-          alternativeText= { this.props.user.name }
-          width= '300'
-          heigth= '300'
-        />
+        <div className="user-profile-card">
+          <p>Name: { this.props.user.name }</p>
+          <p>E-mail: { this.props.user.email }</p>
+          <Image
+            source= { this.props.user.avatar }
+            alternativeText= { this.props.user.name }
+            width= '300'
+            heigth= '300'
+          />
+        </div>
       </div>
     );
   }
