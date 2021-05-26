79861e50f97b49ec842d05403eacdfa3aa1e9db2
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-05-26 09:22:36 -0300

    Add AlbumTable components that creates a table with Albums components

---
+++ exercises/bloco_11/dia_2/my-app/src/components/AlbumTable/AlbumTable.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import Album from '../Album/Album';
+
+class AlbumTable extends Component {
+  render() {
+    const albums = [{
+      image: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Coldplay_-_Mylo_Xyloto.JPG/220px-Coldplay_-_Mylo_Xyloto.JPG',
+      title: 'Mylo Xyloto',
+      releaseDate: {
+        year: '2011',
+        month: '10',
+        day: '24',
+      },
+      others: {
+        recordCompany: 'Capitol, Parlophone',
+        formats: 'CD, digital'
+      }
+    },
+    {
+      image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/5/5d/Coldplay_-_Ghost_Stories.jpg/220px-Coldplay_-_Ghost_Stories.jpg',
+      title: 'Ghost Stories',
+      releaseDate: {
+        year: '2014',
+        month: '05',
+        day: '16',
+      },
+      others: {
+        recordCompany: 'Parlophone',
+        formats: 'CD, digital'
+      }
+    }
+  ];
+
+    return (
+      albums.map(album => <Album album={ album } />)
+    );
+  }
+}
+
+export default AlbumTable;
