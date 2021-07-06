e9a1b6b7d49b4154c5122ba80f143367bd1eea15
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-07-06 08:13:08 -0300

    Add example

---
+++ exercises/bloco_13/01-Ciclo-de-Vida-Componentes/01-Class-Notes/01-Exemplos/randomusers-app/src/PersonCard.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class PersonCard extends Component {
+  render() {
+    const { person: { name, email, age, image} } = this.props;
+
+    return (
+      <div>
+        <p>{name}</p>
+        <p>{email}</p>
+        <p>{age}</p>
+        <img src={image} alt={name}/>
+      </div>
+    );
+  }
+}
