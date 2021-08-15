b83d46152702d7ea4340467dcdef7ed4f0ea30cb
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-08-15 19:05:06 -0300

    Add unfinished exercise

--- exercises/bloco_11/content_review/react-components/my-pokedex/src/components/Pokedex.js
+++ exercises/bloco_11/content_review/react-components/my-pokedex/src/components/Pokedex.js
@@ -3,12 +3,33 @@ import Pokemon from './Pokemon';
 import pokemons from '../data/index';
 
 export default class Pokedex extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentPokemon: 0,
+    };
+
+    this.handleNext = this.handleNext.bind(this);
+  }
+
+  handleNext() {
+    const { currentPokemon } = this.state;
+    if (currentPokemon > pokemons.length) {
+      console.log(currentPokemon, pokemons.length)
+      this.setState({ currentPokemon: 0 });
+    } else {
+      this.setState((prevState) => ({currentPokemon: prevState.currentPokemon + 1}));
+    }
+    console.log(currentPokemon);
+  }
+
   render() {
+    const { currentPokemon } = this.state;
     return (
       <div>
-        {pokemons.map((pokemon) => (
-          <Pokemon key={pokemon.id} pokemon={pokemon} />
-        ))}
+        <Pokemon pokemon={pokemons[currentPokemon]} />
+        <button type="button" onClick={ this.handleNext }>Next Pokemon</button>
       </div>
     );
   }
