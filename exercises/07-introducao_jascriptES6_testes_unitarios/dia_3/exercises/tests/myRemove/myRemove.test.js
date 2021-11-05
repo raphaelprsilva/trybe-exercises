aa2564352dd25dfac4c397a5117a120b68c737c4
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-11-05 17:01:44 -0300

    Add describe block

--- exercises/07-introducao_jascriptES6_testes_unitarios/dia_3/exercises/tests/myRemove/myRemove.test.js
+++ exercises/07-introducao_jascriptES6_testes_unitarios/dia_3/exercises/tests/myRemove/myRemove.test.js
@@ -2,14 +2,16 @@ const myRemove = require('./myRemove');
 
 const FIRST_FOUR_NUMBERS = [1, 2, 3, 4];
 
-test('myRemove([1, 2, 3, 4], 3) should return [1, 2, 4]', () => {
-  expect(myRemove(FIRST_FOUR_NUMBERS, 3)).toEqual([1, 2, 4]);
-});
-
-test('myRemove([1, 2, 3, 4], 3) should not return [1, 2, 3, 4]', () => {
-  expect(myRemove(FIRST_FOUR_NUMBERS, 3)).not.toEqual([1, 2, 3, 4]);
-});
-
-test('myRemove([1, 2, 3, 4], 5) should return [1, 2, 3, 4]', () => {
-  expect(myRemove(FIRST_FOUR_NUMBERS, 5)).toEqual(FIRST_FOUR_NUMBERS);
+describe('Tests myRemove function', () => {
+  test('myRemove([1, 2, 3, 4], 3) should return [1, 2, 4]', () => {
+    expect(myRemove(FIRST_FOUR_NUMBERS, 3)).toEqual([1, 2, 4]);
+  });
+  
+  test('myRemove([1, 2, 3, 4], 3) should not return [1, 2, 3, 4]', () => {
+    expect(myRemove(FIRST_FOUR_NUMBERS, 3)).not.toEqual([1, 2, 3, 4]);
+  });
+  
+  test('myRemove([1, 2, 3, 4], 5) should return [1, 2, 3, 4]', () => {
+    expect(myRemove(FIRST_FOUR_NUMBERS, 5)).toEqual(FIRST_FOUR_NUMBERS);
+  });
 });
