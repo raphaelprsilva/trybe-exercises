e66169f2872d03454be95b6b5cb7a6987be1da66
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-11-06 10:12:41 -0300

    Imlement techList function

--- exercises/07-introducao_jascriptES6_testes_unitarios/dia_3/exercises/tests/techList/techList.js
+++ exercises/07-introducao_jascriptES6_testes_unitarios/dia_3/exercises/tests/techList/techList.js
@@ -1,5 +1,13 @@
-const techList = () => {
-
+const techList = (list, personName) => {
+  if (!list.length) return 'Vazio!'
+  return list
+    .sort()
+    .reduce((acc, tech) => (
+      acc = [...acc, {
+        'name': personName,
+        'tech': tech,
+      }]
+    ), []);
 };
 
 module.exports = techList;
