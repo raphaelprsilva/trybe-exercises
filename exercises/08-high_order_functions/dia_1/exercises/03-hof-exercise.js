5526869d2408a0487d1cf800446cee9ab0ed25df
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-11-10 11:16:03 -0300

    Add exercise 03

--- exercises/08-high_order_functions/dia_1/exercises/03-hof-exercise.js
+++ exercises/08-high_order_functions/dia_1/exercises/03-hof-exercise.js
@@ -1,80 +1,18 @@
-const mage = {
-  healthPoints: 130,
-  intelligence: 45,
-  mana: 102,
-  damage: undefined,
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const checkAnswers = (rightAnswers, studentAnswers) => {
+  return rightAnswers.reduce((acc, answer, index) => {
+    if (answer === studentAnswers[index]) acc += 1;
+    if (answer !== studentAnswers[index] && studentAnswers[index] !== 'N.A') acc += -0.5;
+    if (studentAnswers[index] === 'N.A') acc += 0;
+    return acc;
+  }, 0);
 };
 
-const warrior = {
-  healthPoints: 200,
-  strength: 30,
-  weaponDmg: 2,
-  damage: undefined,
+const getStudentsGrade = (rightAnswers, studentAnswers, callback) => {
+  const finalGrade = callback(rightAnswers, studentAnswers);
+  return `A nota final do aluno é ${finalGrade}`;
 };
 
-const dragon = {
-  healthPoints: 350,
-  strength: 50,
-  damage: undefined,
-};
-
-const battleMembers = { mage, warrior, dragon };
-
-// ---------------------------------------------------------------------------------
-const calculateDamageDragon = (strength) => {
-  const minDamage = 15;
-  const damage = Math.floor(Math.random() * (strength - minDamage) ) + minDamage;
-  return damage;
-};
-
-const dragonStrength = Object.values(battleMembers)[2].strength;
-console.log(calculateDamageDragon(dragonStrength));
-// ---------------------------------------------------------------------------------
-
-// ---------------------------------------------------------------------------------
-const calculateDamageWarrior = (strength, weaponDmg) => {
-  const maxDamage = weaponDmg * strength;
-  const damage = Math.floor(Math.random() * (maxDamage - strength) ) + strength;
-  return damage;
-};
-
-const warriorStrength = Object.values(battleMembers)[1].strength;
-const warriorWeaponDmg = Object.values(battleMembers)[1].weaponDmg;
-
-console.log(calculateDamageWarrior(warriorStrength, warriorWeaponDmg));
-// ---------------------------------------------------------------------------------
-
-const calculateDamageAndManaMage = (inteligence, mana) => {
-  
-  const damageAndMandaStatus = {
-    damageStatus: 'Não possui mana suficiente',
-    manaSpent: 0
-  };
-  
-  if (mana > 15) {
-    const maxDamage = inteligence * 2;
-    const damage = Math.floor(Math.random() * (maxDamage - inteligence) ) + inteligence;
-    const manaSpent = 15;
-    const currentMana = mana - manaSpent;
-    return {
-      damage,
-      currentMana,
-    };
-  }
-  return damageAndMandaStatus;
-};
-
-const mageIntelligence = Object.values(battleMembers)[0].intelligence;
-const mageMana = Object.values(battleMembers)[0].mana;
-
-console.log(calculateDamageAndManaMage(mageIntelligence, mageMana));
-
-const gameActions = {
-  warriorShift: (callback) => {
-    const warriorDamage = callback();
-    warrior.damage = warriorDamage;
-    dragon.healthPoints -= warriorDamage;
-  },
-};
-
-console.log(gameActions.warriorShift(calculateDamageWarrior));
+console.log(getStudentsGrade(RIGHT_ANSWERS, STUDENT_ANSWERS, checkAnswers));
