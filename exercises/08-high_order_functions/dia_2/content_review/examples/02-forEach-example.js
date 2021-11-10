c1df31aa4682a428b9a0032e27055e9c10422b27
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-11-10 13:27:01 -0300

    Add example 02

---
+++ exercises/08-high_order_functions/dia_2/content_review/examples/02-forEach-example.js
@@ -0,0 +1,8 @@
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const multiplicationTable = (numbers, multiplicator) => numbers
+  .forEach((number) => {
+    console.log(`${multiplicator} x ${number} = ${number * multiplicator}`);
+  });
+
+multiplicationTable(numbers, 2);
