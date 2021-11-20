bdc4f875fe7aeb033edce36a44e6910d5e2d8696
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-11-20 12:12:55 -0300

    Add exercise 02

---
+++ exercises/10.1-introducao-react/01-day/02-exercise/src/Footer.jsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import './Footer.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="Footer">
+        <h1 className="Footer-title">E isso é só o começo...</h1>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
---
+++ exercises/10.1-introducao-react/01-day/02-exercise/src/Header.jsx
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="Header">
+        <h1 className="Header-title">Conteúdos de Front-end</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
+
