e4ba24c32df77cb009aa1d606ee4ac035a0ae71c
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-12-07 09:43:33 -0300

    Add exercise 11

--- exercises/20-SQL-introducao/02-dia-02-encontrando-dados/02-exercises/01-exercises.sql
+++ exercises/20-SQL-introducao/02-dia-02-encontrando-dados/02-exercises/01-exercises.sql
@@ -53,6 +53,10 @@ ORDER BY Hours DESC
 LIMIT 3;
 
 -- Escreva uma query para exibir o código de todos os projetos da tabela AssignedTo sem que haja repetições.
+SELECT DISTINCT
+    (Project)
+FROM
+    Scientists.AssignedTo;
 
 -- Escreva uma query para exibir o nome do projeto com maior quantidade de horas.
 
