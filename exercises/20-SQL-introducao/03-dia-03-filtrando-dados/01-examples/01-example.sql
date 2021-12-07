832534d1e7f97247227ffe66aedee837c5b08d92
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-12-07 13:37:32 -0300

    Add example 02

--- exercises/20-SQL-introducao/03-dia-03-filtrando-dados/01-examples/01-example.sql
+++ exercises/20-SQL-introducao/03-dia-03-filtrando-dados/01-examples/01-example.sql
@@ -18,7 +18,16 @@ FROM
 WHERE
     email = 'LEONARD.SCHOFIELD@sakilacustomer.org';
 
--- 2. Precisamos de um relatório dos nomes dos clientes, em `ordem alfabética` , que não estão mais ativos no nosso sistema e pertencem à loja com o **id = 2** , e não inclua o cliente `KENNETH` no resultado. As informações podem ser encontradas na tabela `customer`
+-- 2. Precisamos de um relatório dos nomes dos clientes, em `ordem alfabética` , que não estão mais ativos no nosso sistema e pertencem à loja com o **id = 2** , e não inclua o cliente `KENNETH` no resultado. As informações podem ser encontradas na tabela `customer`;
+SELECT 
+    *
+FROM
+    sakila.customer
+WHERE
+    active IS FALSE
+    AND store_id = 2
+    AND first_name <> 'KENNETH'
+ORDER BY first_name;
 
 
 -- 3. O setor financeiro quer saber título, descrição, ano de lançamento e valor do custo de substituição ( *replacement_cost* ), dos 100 filmes com o maior custo de substituição, do valor mais alto ao mais baixo, entre os filmes feitos para menores de idade e que têm o custo mínimo de substituição de $18,00 dólares. Em caso de empate, ordene em `ordem alfabética` pelo título. As informações podem ser encontradas na tabela `film`
