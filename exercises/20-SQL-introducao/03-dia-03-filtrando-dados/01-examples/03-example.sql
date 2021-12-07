d3a332a388e67600839f8b47340f91489128ddc5
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-12-07 16:14:22 -0300

    Add example 07

--- exercises/20-SQL-introducao/03-dia-03-filtrando-dados/01-examples/03-example.sql
+++ exercises/20-SQL-introducao/03-dia-03-filtrando-dados/01-examples/03-example.sql
@@ -50,4 +50,10 @@ WHERE
         OR title LIKE 'mosquito%';
 
 -- 7. Mostre os seis filmes que contêm as palavras `monkey` e `sumo` em suas descrições.
-
+SELECT 
+    *
+FROM
+    sakila.film
+WHERE
+    description LIKE '%monkey%'
+        AND description LIKE '%sumo%';
