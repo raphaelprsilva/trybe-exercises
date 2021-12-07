27f2600bbe8e5e9ac0485b376571ac2b4b0331fb
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-12-07 09:51:25 -0300

    Add exercise 15

--- exercises/20-SQL-introducao/02-dia-02-encontrando-dados/02-exercises/01-exercises.sql
+++ exercises/20-SQL-introducao/02-dia-02-encontrando-dados/02-exercises/01-exercises.sql
@@ -83,4 +83,10 @@ ORDER BY Hours
 LIMIT 5;
 
 -- Escreva uma query que exiba a string "Existem Number cientistas na tabela Scientists.", em que Number se refira a quantidade de cientistas.
-
+SELECT 
+    CONCAT('Existem ',
+            COUNT(*),
+            ' cientistas na tabela Scientists.')
+	AS 'Núm. de Cientistas'
+FROM
+    Scientists.Scientists;
