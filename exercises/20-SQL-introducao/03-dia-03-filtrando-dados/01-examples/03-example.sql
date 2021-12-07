b3be1c488b0b396908cbd5b4e24253b68c212af8
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-12-07 15:45:55 -0300

    Add example 01

---
+++ exercises/20-SQL-introducao/03-dia-03-filtrando-dados/01-examples/03-example.sql
@@ -0,0 +1,25 @@
+-- 1. Mostre todos os detalhes dos filmes que contêm a palavra `ace` no nome.
+SELECT 
+    *
+FROM
+    sakila.film
+WHERE
+    title LIKE '%ace%';
+
+-- 2. Mostre todos os detalhes dos filmes cujas descrições finalizam com `china` .
+
+
+-- 3. Mostre todos os detalhes dos dois filmes cujas descrições contêm a palavra `girl` e o título finaliza com a palavra `lord` .
+
+
+-- 4. Mostre os dois casos em que, a partir do 4° caractere no título do filme, tem-se a palavra `gon` .
+
+
+-- 5. Mostre o único caso em que, a partir do 4° caractere no título do filme, tem-se a palavra `gon` e a descrição contém a palavra `Documentary` .
+
+
+-- 6. Mostre os dois filmes cujos títulos ou finalizam com `academy` ou iniciam com `mosquito` .
+
+
+-- 7. Mostre os seis filmes que contêm as palavras `monkey` e `sumo` em suas descrições.
+
