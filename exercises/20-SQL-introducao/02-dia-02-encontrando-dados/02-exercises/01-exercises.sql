e92967224b0b058045a51816736699695257f64c
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-12-07 09:45:25 -0300

    Add exercise 12

--- exercises/20-SQL-introducao/02-dia-02-encontrando-dados/02-exercises/01-exercises.sql
+++ exercises/20-SQL-introducao/02-dia-02-encontrando-dados/02-exercises/01-exercises.sql
@@ -59,6 +59,12 @@ FROM
     Scientists.AssignedTo;
 
 -- Escreva uma query para exibir o nome do projeto com maior quantidade de horas.
+SELECT 
+    Name
+FROM
+    Scientists.Projects
+ORDER BY Hours DESC
+LIMIT 1;
 
 -- Escreva uma query para exibir o nome do segundo projeto com menor quantidade de horas.
 
