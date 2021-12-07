15cbeda3da7b4266e6021fde997dde07c7ff4658
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-12-07 15:54:17 -0300

    Add example 02

--- exercises/20-SQL-introducao/03-dia-03-filtrando-dados/01-examples/03-example.sql
+++ exercises/20-SQL-introducao/03-dia-03-filtrando-dados/01-examples/03-example.sql
@@ -7,7 +7,12 @@ WHERE
     title LIKE '%ace%';
 
 -- 2. Mostre todos os detalhes dos filmes cujas descrições finalizam com `china` .
-
+SELECT 
+    *
+FROM
+    sakila.film
+WHERE
+    description LIKE '%china';
 
 -- 3. Mostre todos os detalhes dos dois filmes cujas descrições contêm a palavra `girl` e o título finaliza com a palavra `lord` .
 
