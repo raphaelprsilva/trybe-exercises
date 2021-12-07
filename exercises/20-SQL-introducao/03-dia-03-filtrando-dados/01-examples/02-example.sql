47764ecc8da3eb8b6d85ea4d7eb33046277b3361
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-12-07 15:37:23 -0300

    Add example 05

--- exercises/20-SQL-introducao/03-dia-03-filtrando-dados/01-examples/02-example.sql
+++ exercises/20-SQL-introducao/03-dia-03-filtrando-dados/01-examples/02-example.sql
@@ -31,7 +31,12 @@ WHERE
     title LIKE 'p%l';
 
 -- Encontra qualquer resultado em que o segundo caractere da frase é "C"
-
+SELECT 
+    *
+FROM
+    sakila.film
+WHERE
+    title LIKE '_c%';
 
 -- Encontra qualquer resultado em que o título possui exatamente 8 caracteres
 
