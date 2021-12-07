72ff31c7bbc746bb1eacce87ac47a3a896621d48
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-12-07 15:28:40 -0300

    Add example 01

---
+++ exercises/20-SQL-introducao/03-dia-03-filtrando-dados/01-examples/02-example.sql
@@ -0,0 +1,24 @@
+-- Encontra qualquer resultado finalizando com "don"
+SELECT 
+    *
+FROM
+    sakila.film
+WHERE
+    title LIKE '%don';
+
+-- Encontra qualquer resultado iniciando com "plu"
+
+
+-- Encontra qualquer resultado que contém "plu"
+
+
+-- Encontra qualquer resultado que inicia com "p" e finaliza com "r"
+
+
+-- Encontra qualquer resultado em que o segundo caractere da frase é "C"
+
+
+-- Encontra qualquer resultado em que o título possui exatamente 8 caracteres
+
+
+-- Encontra todas as palavras com no mínimo 3 caracteres e que iniciam com E
