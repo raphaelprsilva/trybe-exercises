417a8ac12348e513690317d159bc8b55cf813837
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-12-08 09:08:21 -0300

    Add example 02

--- exercises/20-SQL-introducao/03-dia-03-filtrando-dados/01-examples/05-example.sql
+++ exercises/20-SQL-introducao/03-dia-03-filtrando-dados/01-examples/05-example.sql
@@ -13,7 +13,13 @@ WHERE
 ORDER BY first_name;
 
 -- 2. Mostre o e-mail dos clientes com os `address_id 172, 173, 174, 175 e 176` , ordenados em ordem alfabética.
-
+SELECT 
+    email
+FROM
+    sakila.customer
+WHERE
+    address_id IN (172 , 173, 174, 175, 176)
+ORDER BY email;
 
 -- 3. Descubra quantos pagamentos foram feitos entre `01/05/2005` e `01/08/2005` . Lembre-se de que, no banco de dados, as datas estão armazenadas no formato `ano/mês/dia` , diferente do formato brasileiro, que é `dia/mês/ano` .
 
