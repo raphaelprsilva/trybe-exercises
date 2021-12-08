bc0bb1d2191c63bdbbd67fe179c8eb83c7a10c57
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-12-08 09:21:20 -0300

    Add example 05

--- exercises/20-SQL-introducao/03-dia-03-filtrando-dados/01-examples/05-example.sql
+++ exercises/20-SQL-introducao/03-dia-03-filtrando-dados/01-examples/05-example.sql
@@ -39,4 +39,11 @@ WHERE
 ORDER BY rental_duration DESC , title;
 
 -- 5. Monte um relatório que exiba o título e classificação dos `500` primeiros filmes direcionados para as classificações indicativas `G, PG e PG-13` . Os resultados devem estar `ordenados` por título.
-
+SELECT 
+    title, rating
+FROM
+    sakila.film
+WHERE
+    rating IN ('G' , 'PG', 'PG-13')
+ORDER BY title
+LIMIT 500;
