e8c7a313c125f924cd46a06efcd0fe5ffd450c0c
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-12-08 09:00:57 -0300

    Add example file

---
+++ exercises/20-SQL-introducao/03-dia-03-filtrando-dados/01-examples/05-example.sql
@@ -0,0 +1,14 @@
+-- 1. Mostre o nome, sobrenome e e-mail dos clientes com os seguintes sobrenomes: `hicks, crawford, henry, boyd, mason, morales e kennedy` , ordenados por nome em ordem alfabética.
+
+
+-- 2. Mostre o e-mail dos clientes com os `address_id 172, 173, 174, 175 e 176` , ordenados em ordem alfabética.
+
+
+-- 3. Descubra quantos pagamentos foram feitos entre `01/05/2005` e `01/08/2005` . Lembre-se de que, no banco de dados, as datas estão armazenadas no formato `ano/mês/dia` , diferente do formato brasileiro, que é `dia/mês/ano` .
+
+
+-- 4. Mostre o título, ano de lançamento e duração do empréstimo de todos os filmes com a duração de empréstimo de `3 a 6` . Os resultados devem ser classificados em filmes com `maior duração` de empréstimo primeiro. Em caso de empate, ordene em `ordem alfabética` pelo título.
+
+
+-- 5. Monte um relatório que exiba o título e classificação dos `500` primeiros filmes direcionados para as classificações indicativas `G, PG e PG-13` . Os resultados devem estar `ordenados` por título.
+
