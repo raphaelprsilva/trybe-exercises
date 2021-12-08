2b0d810cb04b96f52eeda9749a224ed201e57410
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-12-08 10:12:39 -0300

    Add exercise 03

--- exercises/20-SQL-introducao/03-dia-03-filtrando-dados/02-exercises/01-exercises.sql
+++ exercises/20-SQL-introducao/03-dia-03-filtrando-dados/02-exercises/01-exercises.sql
@@ -16,7 +16,12 @@ WHERE
 ORDER BY Fornecedor;
 
 -- 03. Escreva uma query para exibir as peças, preço e fornecedor de todos os fornecimentos em que o código do fornecedor tenha a letra N .
-
+SELECT 
+    peca, Preco, Fornecedor
+FROM
+    PecasFornecedores.Fornecimentos
+WHERE
+    Fornecedor LIKE '%N%';
 
 -- 04. Escreva uma query para exibir todas as informações dos fornecedores que são empresas limitadas (LTDA). Ordene os resultados em ordem alfabética decrescente.
 
