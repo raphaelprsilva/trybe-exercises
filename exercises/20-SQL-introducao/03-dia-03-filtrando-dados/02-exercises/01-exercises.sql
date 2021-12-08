a172573aad6e1bb2dfb650dd5d00fc16b5429856
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-12-08 10:06:23 -0300

    Add exercise 01

---
+++ exercises/20-SQL-introducao/03-dia-03-filtrando-dados/02-exercises/01-exercises.sql
@@ -0,0 +1,24 @@
+-- 01. Escreva uma query para exibir todas as peças que começam com GR .
+SELECT 
+    *
+FROM
+    PecasFornecedores.Pecas
+WHERE
+    name LIKE 'GR%';
+
+-- 02. Escreva uma query para exibir todos os fornecimentos que contenham a peça com code 2 . Organize o resultado por ordem alfabética de fornecedor.
+
+
+-- 03. Escreva uma query para exibir as peças, preço e fornecedor de todos os fornecimentos em que o código do fornecedor tenha a letra N .
+
+
+-- 04. Escreva uma query para exibir todas as informações dos fornecedores que são empresas limitadas (LTDA). Ordene os resultados em ordem alfabética decrescente.
+
+
+-- 05. Escreva uma query para exibir o número de empresas (fornecedores) que contém a letra F no código.
+
+
+-- 06. Escreva uma query para exibir os fornecimentos onde as peças custam mais de R$15,00 e menos de $40,00 . Ordene os resultados por ordem crescente.
+
+
+-- 07. Escreva uma query para exibir o número de vendas feitas entre o dia 15/04/2018 e o dia 30/07/2019 .
