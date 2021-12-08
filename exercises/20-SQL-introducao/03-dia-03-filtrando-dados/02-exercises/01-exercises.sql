07ade7bfc7c34e868e7b610ad1b0173921fb389b
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-12-08 10:18:30 -0300

    Add exercise 07

--- exercises/20-SQL-introducao/03-dia-03-filtrando-dados/02-exercises/01-exercises.sql
+++ exercises/20-SQL-introducao/03-dia-03-filtrando-dados/02-exercises/01-exercises.sql
@@ -41,6 +41,11 @@ WHERE
     code LIKE '%f%';
 
 -- 06. Escreva uma query para exibir os fornecimentos onde as peças custam mais de R$15,00 e menos de $40,00 . Ordene os resultados por ordem crescente.
-
+SELECT 
+    *
+FROM
+    PecasFornecedores.Fornecimentos
+WHERE
+    Preco BETWEEN 15 AND 40;
 
 -- 07. Escreva uma query para exibir o número de vendas feitas entre o dia 15/04/2018 e o dia 30/07/2019 .
