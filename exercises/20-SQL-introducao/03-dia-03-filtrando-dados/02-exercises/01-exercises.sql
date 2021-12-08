5b5ac46b94d1cc8ead083a7948cacb605cfcaff3
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-12-08 10:09:56 -0300

    Add exercise 02

--- exercises/20-SQL-introducao/03-dia-03-filtrando-dados/02-exercises/01-exercises.sql
+++ exercises/20-SQL-introducao/03-dia-03-filtrando-dados/02-exercises/01-exercises.sql
@@ -7,7 +7,13 @@ WHERE
     name LIKE 'GR%';
 
 -- 02. Escreva uma query para exibir todos os fornecimentos que contenham a peça com code 2 . Organize o resultado por ordem alfabética de fornecedor.
-
+SELECT 
+    *
+FROM
+    PecasFornecedores.Fornecimentos
+WHERE
+    peca = 2
+ORDER BY Fornecedor;
 
 -- 03. Escreva uma query para exibir as peças, preço e fornecedor de todos os fornecimentos em que o código do fornecedor tenha a letra N .
 
