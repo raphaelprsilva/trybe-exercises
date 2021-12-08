20f2a296c4f5fe68801925f107b5d9cb7719eb7e
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-12-08 10:20:31 -0300

    Add exercise 07

--- exercises/20-SQL-introducao/03-dia-03-filtrando-dados/02-exercises/01-exercises.sql
+++ exercises/20-SQL-introducao/03-dia-03-filtrando-dados/02-exercises/01-exercises.sql
@@ -49,3 +49,9 @@ WHERE
     Preco BETWEEN 15 AND 40;
 
 -- 07. Escreva uma query para exibir o número de vendas feitas entre o dia 15/04/2018 e o dia 30/07/2019 .
+SELECT 
+    COUNT(*)
+FROM
+    PecasFornecedores.Vendas
+WHERE
+    DATE(order_date) BETWEEN '2018-04-15' AND '2019-07-30';
