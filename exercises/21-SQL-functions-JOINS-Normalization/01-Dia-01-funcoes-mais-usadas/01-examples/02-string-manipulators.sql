752e62425010dd6bdb9b48d9d6b5454b8154ea7c
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-12-09 16:06:55 -0300

    Add remaining examples

--- exercises/21-SQL-functions-JOINS-Normalization/01-Dia-01-funcoes-mais-usadas/01-examples/02-string-manipulators.sql
+++ exercises/21-SQL-functions-JOINS-Normalization/01-Dia-01-funcoes-mais-usadas/01-examples/02-string-manipulators.sql
@@ -11,16 +11,13 @@ SELECT REPLACE(title, 'ACADEMY', 'TESTE') FROM sakila.film WHERE film_id = 1;
 SELECT LEFT(title, 7) FROM sakila.film WHERE film_id = 1;
 
 -- Example 05
-
+SELECT RIGHT(title, 8) FROM sakila.film WHERE film_id = 1;
 
 -- Example 06
-
+SELECT CHAR_LENGTH(title) FROM sakila.film WHERE film_id = 1;
 
 -- Example 07
-
+SELECT SUBSTRING(title, 5, 2) FROM sakila.film WHERE film_id = 1;
 
 -- Example 08
-
-
--- Example 09
-
+SELECT SUBSTRING(title, 5) FROM sakila.film WHERE film_id = 1;
