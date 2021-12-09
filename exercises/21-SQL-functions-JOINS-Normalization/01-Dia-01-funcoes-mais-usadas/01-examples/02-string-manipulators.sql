d01d5838584591ab6af2d75b6d251077deecded6
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-12-09 15:42:05 -0300

    Add example 01

---
+++ exercises/21-SQL-functions-JOINS-Normalization/01-Dia-01-funcoes-mais-usadas/01-examples/02-string-manipulators.sql
@@ -0,0 +1,26 @@
+-- Example 01
+SELECT UCASE(title) FROM sakila.film LIMIT 10;
+
+-- Example 02
+
+
+-- Example 03
+
+
+-- Example 04
+
+
+-- Example 05
+
+
+-- Example 06
+
+
+-- Example 07
+
+
+-- Example 08
+
+
+-- Example 09
+
