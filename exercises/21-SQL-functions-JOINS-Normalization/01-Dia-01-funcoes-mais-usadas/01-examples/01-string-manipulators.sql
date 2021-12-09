5b1d17698f28e04dc5324551c81414533bf9567b
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-12-09 15:26:52 -0300

    Add example file

---
+++ exercises/21-SQL-functions-JOINS-Normalization/01-Dia-01-funcoes-mais-usadas/01-examples/01-string-manipulators.sql
@@ -0,0 +1,27 @@
+-- Converte o texto da string para CAIXA ALTA
+
+
+-- Converte o texto da string para caixa baixa
+
+
+-- Substitui as ocorrências de uma substring em uma string
+
+
+-- Retorna a parte da esquerda de uma string de acordo com o
+-- número de caracteres especificado
+
+
+-- Retorna a parte da direita de uma string de acordo com o
+-- número de caracteres especificado
+
+
+-- Exibe o tamanho, em caracteres, da string, a função LENGTH retorna o tamanho em bytes
+
+
+-- Extrai parte de uma string de acordo com o índice de um caractere inicial
+-- e a quantidade de caracteres a extrair
+
+
+-- Se a quantidade de caracteres a extrair não for definida,
+-- então a string será extraída do índice inicial definido, até o seu final
+
