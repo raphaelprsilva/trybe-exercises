7a32615048076f559e9730799856f39c8b2c6ff9
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-12-13 08:05:02 -0300

    Add exercise 04

--- exercises/21-SQL-functions-JOINS-Normalization/01-Dia-01-funcoes-mais-usadas/02-exercises/01-exercises.sql
+++ exercises/21-SQL-functions-JOINS-Normalization/01-Dia-01-funcoes-mais-usadas/02-exercises/01-exercises.sql
@@ -16,7 +16,10 @@ GROUP BY JOB_ID
 ORDER BY salary_avg DESC;
 
 -- 4. Escreva uma query que exiba a quantidade de dinheiro necessária para realizar o pagamento de todas as pessoas funcionárias.
-
+SELECT 
+    SUM(SALARY) AS amount_salaries
+FROM
+    hr.employees;
 
 -- 5. Escreva uma query que exiba quatro informações: o maior salário, o menor salário, a soma de todos os salários e a média dos salários. Todos os valores devem ser formatados para ter apenas duas casas decimais.
 
