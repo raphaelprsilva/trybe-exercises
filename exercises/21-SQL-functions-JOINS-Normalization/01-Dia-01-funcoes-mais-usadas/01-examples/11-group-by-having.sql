6ba44a351a4892c450ba0bf5854c3747c807c699
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-12-13 07:43:54 -0300

    Add example 02

--- exercises/21-SQL-functions-JOINS-Normalization/01-Dia-01-funcoes-mais-usadas/01-examples/11-group-by-having.sql
+++ exercises/21-SQL-functions-JOINS-Normalization/01-Dia-01-funcoes-mais-usadas/01-examples/11-group-by-having.sql
@@ -13,3 +13,10 @@ ORDER BY media_duracao_filme DESC;
 -- 2. Usando a query a seguir, exiba apenas os valores de total do custo de
 -- substituição que estão acima de $3950.50. Dê um alias que faça sentido para
 -- SUM(replacement_cost) , de forma que deixe a query mais legível. Finalize ordenando os resultados de forma crescente.
+SELECT 
+    rating, SUM(replacement_cost) AS total_amount
+FROM
+    sakila.film
+GROUP BY rating
+HAVING total_amount > 3950.5
+ORDER BY total_amount;
