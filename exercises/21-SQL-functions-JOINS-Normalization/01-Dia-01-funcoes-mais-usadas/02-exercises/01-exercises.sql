0e833c6961e774f4944b4b2509788790db9beb6d
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-12-13 07:54:02 -0300

    Add exercise 02

--- exercises/21-SQL-functions-JOINS-Normalization/01-Dia-01-funcoes-mais-usadas/02-exercises/01-exercises.sql
+++ exercises/21-SQL-functions-JOINS-Normalization/01-Dia-01-funcoes-mais-usadas/02-exercises/01-exercises.sql
@@ -2,7 +2,10 @@
 SELECT FIRST_NAME, MAX(SALARY) FROM hr.employees;
 
 -- 2. Escreva uma query que exiba a diferença entre o maior e o menor salário.
-
+SELECT 
+    MAX(SALARY) - MIN(SALARY) AS diff_max_min_salary
+FROM
+    hr.employees;
 
 -- 3. Escreva uma query que exiba a média salarial de cada `JOB_ID` , ordenando pela média salarial em ordem decrescente.
 
