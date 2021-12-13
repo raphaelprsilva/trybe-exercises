607f066d73c2a32b6b0f8e2a29696f18550daf03
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-12-13 07:57:30 -0300

    Add exercise 03

--- exercises/21-SQL-functions-JOINS-Normalization/01-Dia-01-funcoes-mais-usadas/02-exercises/01-exercises.sql
+++ exercises/21-SQL-functions-JOINS-Normalization/01-Dia-01-funcoes-mais-usadas/02-exercises/01-exercises.sql
@@ -8,7 +8,12 @@ FROM
     hr.employees;
 
 -- 3. Escreva uma query que exiba a média salarial de cada `JOB_ID` , ordenando pela média salarial em ordem decrescente.
-
+SELECT 
+    JOB_ID, AVG(SALARY) AS salary_avg
+FROM
+    hr.employees
+GROUP BY JOB_ID
+ORDER BY salary_avg DESC;
 
 -- 4. Escreva uma query que exiba a quantidade de dinheiro necessária para realizar o pagamento de todas as pessoas funcionárias.
 
