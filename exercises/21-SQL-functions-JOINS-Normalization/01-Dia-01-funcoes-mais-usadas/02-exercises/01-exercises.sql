6dca317157a25a0b06660802fd7b454f63a005a0
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-12-13 08:51:17 -0300

    Add exercise 12

--- exercises/21-SQL-functions-JOINS-Normalization/01-Dia-01-funcoes-mais-usadas/02-exercises/01-exercises.sql
+++ exercises/21-SQL-functions-JOINS-Normalization/01-Dia-01-funcoes-mais-usadas/02-exercises/01-exercises.sql
@@ -81,7 +81,12 @@ WHERE
     PHONE_NUMBER LIKE '515%';
 
 -- 12. Escreva uma query que só exiba as informações dos funcionários cujo o primeiro nome tenha oito ou mais caracteres.
-
+SELECT 
+    *
+FROM
+    hr.employees
+WHERE
+    CHAR_LENGTH(FIRST_NAME) >= 8;
 
 -- 13. Escreva uma query que exiba as seguintes informações de cada funcionário: `id` , `primeiro nome` e `ano no qual foi contratado` (exiba somente o ano).
 
