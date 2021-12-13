3a35f58a48960aae93b08de9664175462ed1cb63
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-12-13 08:52:53 -0300

    Add exercise 13

--- exercises/21-SQL-functions-JOINS-Normalization/01-Dia-01-funcoes-mais-usadas/02-exercises/01-exercises.sql
+++ exercises/21-SQL-functions-JOINS-Normalization/01-Dia-01-funcoes-mais-usadas/02-exercises/01-exercises.sql
@@ -89,7 +89,10 @@ WHERE
     CHAR_LENGTH(FIRST_NAME) >= 8;
 
 -- 13. Escreva uma query que exiba as seguintes informações de cada funcionário: `id` , `primeiro nome` e `ano no qual foi contratado` (exiba somente o ano).
-
+SELECT 
+    EMPLOYEE_ID, FIRST_NAME, HIRE_DATE
+FROM
+    hr.employees;
 
 -- 14. Escreva uma query que exiba as seguintes informações de cada funcionário: `id` , `primeiro nome` e `dia do mês no qual foi contratado` (exiba somente o dia).
 
