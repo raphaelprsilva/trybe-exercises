e32ca92cb231da75e573462c92624491637d5700
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-12-13 10:08:32 -0300

    Add example 03

--- exercises/21-SQL-functions-JOINS-Normalization/02-Dia-02-descomplicando-joins/01-examples/01-example.sql
+++ exercises/21-SQL-functions-JOINS-Normalization/02-Dia-02-descomplicando-joins/01-examples/01-example.sql
@@ -16,7 +16,20 @@ ON staff.address_id = address.address_id;
 -- clientes, ordenados pelo nome em ordem decrescente, juntamente com o id do
 -- endereço e o nome da rua onde o cliente mora. Essas informações podem
 -- ser encontradas nas tabelas `customer` e `address` .
-
+SELECT 
+    customer.customer_id,
+    CONCAT(customer.first_name,
+            ' ',
+            customer.last_name) AS full_name,
+    customer.email,
+    address.address_id,
+    address.address
+FROM
+    sakila.customer AS customer
+        INNER JOIN
+    sakila.address AS address ON customer.customer_id = customer.customer_id
+ORDER BY full_name DESC
+LIMIT 100;
 
 -- 4. Exiba o nome , email , id do endereço , endereço e distrito dos clientes
 -- que moram no distrito da California e que contêm "rene" em seus nomes.
