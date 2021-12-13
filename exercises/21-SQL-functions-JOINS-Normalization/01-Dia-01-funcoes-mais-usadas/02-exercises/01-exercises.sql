f5123d08e7a31c0011410dde9dd6bb23283fff67
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-12-13 08:38:07 -0300

    Add exercise 09

--- exercises/21-SQL-functions-JOINS-Normalization/01-Dia-01-funcoes-mais-usadas/02-exercises/01-exercises.sql
+++ exercises/21-SQL-functions-JOINS-Normalization/01-Dia-01-funcoes-mais-usadas/02-exercises/01-exercises.sql
@@ -55,7 +55,13 @@ GROUP BY JOB_ID
 HAVING JOB_ID = 'IT_PROG';
 
 -- 9. Escreva uma query que exiba em ordem decrescente a média salarial de todos os cargos, exceto das pessoas programadoras ( `IT_PROG` ).
-
+SELECT 
+    JOB_ID, AVG(SALARY) AS SALARY_AVG
+FROM
+    hr.employees
+GROUP BY JOB_ID
+HAVING JOB_ID <> 'IT_PROG'
+ORDER BY SALARY_AVG DESC;
 
 -- 10. Escreva um query que exiba média salarial e o número de funcionários de todos os departamentos com mais de dez funcionários. Dica: agrupe pelo `department_id` .
 
