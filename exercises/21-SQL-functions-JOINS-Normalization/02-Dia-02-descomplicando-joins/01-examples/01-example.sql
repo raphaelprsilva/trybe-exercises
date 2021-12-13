26f1422a1037b4813868d873eef2493d120c8c5a
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-12-13 13:45:39 -0300

    Add example 07

--- exercises/21-SQL-functions-JOINS-Normalization/02-Dia-02-descomplicando-joins/01-examples/01-example.sql
+++ exercises/21-SQL-functions-JOINS-Normalization/02-Dia-02-descomplicando-joins/01-examples/01-example.sql
@@ -80,4 +80,12 @@ GROUP BY staff.staff_id;
 
 -- 7. Monte uma query que exiba o id do ator , nome , id do filme e título do
 -- filme , usando as tabelas `actor` , `film_actor` e `film` . Dica: você
--- precisará fazer mais de um `JOIN` na mesma query .
+-- precisará fazer mais de um `JOIN` na mesma query.
+SELECT 
+    A.actor_id, A.first_name, F.film_id, F.title
+FROM
+    sakila.actor AS A
+        INNER JOIN
+    sakila.film_actor AS FA ON A.actor_id = FA.actor_id
+        INNER JOIN
+    sakila.film AS F ON FA.film_id = F.film_id;
