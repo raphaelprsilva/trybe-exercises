e0ff485eebe740f62ab01fbdf8327a008544f4ef
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-12-13 06:39:21 -0300

    Add example 01 and 02

---
+++ exercises/21-SQL-functions-JOINS-Normalization/01-Dia-01-funcoes-mais-usadas/01-examples/09-functions.sql
@@ -0,0 +1,18 @@
+-- 1. Monte um query que exiba:
+-- - A média de duração dos filmes e dê o nome da coluna de 'Média de Duração';
+SELECT 
+    AVG(length) AS 'Média de Duração'
+FROM
+    sakila.film;
+
+-- - A duração mínima dos filmes como 'Duração Mínima';
+SELECT 
+    MIN(length) AS 'Duração Mínima'
+FROM
+    sakila.film;
+
+-- - A duração máxima dos filmes como 'Duração Máxima';
+
+-- - A soma de todas as durações como 'Tempo de Exibição Total';
+
+-- - E finalmente, a quantidade total de filmes cadastrados na tabela `sakila.film` como 'Filmes Registrados'.
