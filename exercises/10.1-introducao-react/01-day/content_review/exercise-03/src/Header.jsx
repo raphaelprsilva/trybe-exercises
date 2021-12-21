e85848882badc4cf528b98004ba5b91796473d47
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-12-21 05:47:17 -0300

    Add exercise 03

---
+++ exercises/10.1-introducao-react/01-day/content_review/exercise-03/src/Header.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className='header'>
+        <h1>
+          Conteúdos de Front-end
+        </h1>
+      </header>
+    )
+  }
+}
+
+export default Header;
