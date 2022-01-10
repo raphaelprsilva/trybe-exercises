466a8b0f13458952000e69ea87edbe132ee34cec
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-01-10 07:52:59 -0300

    Change file writeFile.js to async/await syntax

--- exercises/22-intro-node-js/02-day-async-flow/01-examples/03-example/writeFile.js
+++ exercises/22-intro-node-js/02-day-async-flow/01-examples/03-example/writeFile.js
@@ -1,9 +1,12 @@
 const fs = require('fs').promises;
 
-fs.writeFile('./meu-arquivo.txt', 'Meu textão')
-  .then(() => {
-    console.log('Arquivo escrito com sucesso!');
-  })
-  .catch((err) => {
-    console.error(`Erro ao escrever o arquivo: ${err.message}`);
-  });
+async function main() {
+  try {
+    await fs.writeFile('./meu-arquivo.txt', 'Novo texto escrito!');
+    console.log('Texto escrito com sucesso!');
+  } catch (err) {
+    console.log(`Erro ao escrever o arquivo: ${err.message}`);
+  }
+}
+
+main();
