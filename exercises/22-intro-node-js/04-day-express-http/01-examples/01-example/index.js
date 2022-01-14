f4ca71594dd276c50359e3c2a6eb95efe09be009
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-01-14 16:29:48 -0300

    Add request method all that returns an error independent of method error

--- exercises/22-intro-node-js/04-day-express-http/01-examples/01-example/index.js
+++ exercises/22-intro-node-js/04-day-express-http/01-examples/01-example/index.js
@@ -74,6 +74,10 @@ app.delete('/recipes/:id', (req, res) => {
   res.status(204).end();
 });
 
+app.all('*', (req, res) => {
+  return res.status(404).json({ message: `Rota '${req.path}' não existe.` });
+});
+
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
 });
