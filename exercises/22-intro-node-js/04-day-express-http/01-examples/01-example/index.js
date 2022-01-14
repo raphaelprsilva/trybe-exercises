a63d7f1e4ae7122f3f72765c6f6018c42c9e870a
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-01-14 09:28:05 -0300

    Add post request method to add new recipe

--- exercises/22-intro-node-js/04-day-express-http/01-examples/01-example/index.js
+++ exercises/22-intro-node-js/04-day-express-http/01-examples/01-example/index.js
@@ -1,13 +1,16 @@
-const express = require("express");
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.json());
 
 const recipes = [
-  { id: 1, name: "Lasanha", price: 40.0, waitTime: 30 },
-  { id: 2, name: "Macarrão a Bolonhesa", price: 35.0, waitTime: 25 },
-  { id: 3, name: "Macarrão com molho branco", price: 35.0, waitTime: 25 },
+  { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
+  { id: 2, name: 'Macarrão a Bolonhesa', price: 35.0, waitTime: 25 },
+  { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
 ];
 
-app.get("/recipes/search", (req, res) => {
+app.get('/recipes/search', (req, res) => {
   const { name, maxPrice } = req.query;
   const filteredRecipes = recipes.filter(
     (r) => r.name.includes(name) && r.price < parseInt(maxPrice)
@@ -15,16 +18,22 @@ app.get("/recipes/search", (req, res) => {
   res.status(200).json(filteredRecipes);
 });
 
-app.get("/recipes/:id", (req, res) => {
-  console.log("req.params:", req.params);
+app.get('/recipes/:id', (req, res) => {
+  console.log('req.params:', req.params);
   const { id } = req.params;
   const recipe = recipes.find((r) => r.id === parseInt(id));
 
-  if (!recipe) return res.status(404).json({ message: "Recipe not found!" });
+  if (!recipe) return res.status(404).json({ message: 'Recipe not found!' });
 
   res.status(200).json(recipe);
 });
 
+app.post('/recipes', (req, res) => {
+  const { id, name, price } = req.body;
+  recipes.push({ id, name, price });
+  res.status(201).json({ message: 'Recipe created successfully!' });
+});
+
 app.listen(3001, () => {
-  console.log("Aplicação ouvindo na porta 3001");
+  console.log('Aplicação ouvindo na porta 3001');
 });
