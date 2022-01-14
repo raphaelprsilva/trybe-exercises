a4a51b8a4249e971d4c9af1cb8f8016f08aaf55f
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-01-14 17:06:03 -0300

    Add exercise 02

--- exercises/22-intro-node-js/04-day-express-http/02-exercises/01-exercise/index.js
+++ exercises/22-intro-node-js/04-day-express-http/02-exercises/01-exercise/index.js
@@ -5,6 +5,11 @@ app.get('/ping', (req, res) => {
   res.status(200).json({ message: 'pong' });
 });
 
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  res.status(200).json({ message: `Hello, ${name}!` });
+});
+
 app.listen(3001, () => {
   console.log(`Escutando na porta 3001`);
 });
