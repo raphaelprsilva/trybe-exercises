c69eb576ac67028b571f50c400e9172effb57653
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-01-14 09:45:21 -0300

    Add request method GET that validade token

--- exercises/22-intro-node-js/04-day-express-http/01-examples/01-example/index.js
+++ exercises/22-intro-node-js/04-day-express-http/01-examples/01-example/index.js
@@ -34,6 +34,14 @@ app.post('/recipes', (req, res) => {
   res.status(201).json({ message: 'Recipe created successfully!' });
 });
 
+app.get('/validateToken', (req, res) => {
+  const token = req.headers.authorization;
+  if (token.length !== 16)
+    return res.status(401).json({ message: 'Invalid Token!' });
+
+  res.status(200).json({ message: 'Valid Token!' });
+});
+
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
 });
