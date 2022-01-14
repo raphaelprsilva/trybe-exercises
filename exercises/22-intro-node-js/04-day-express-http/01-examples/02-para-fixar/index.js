fd3ad1f3ba7d72afa4b0b6adfe6dc801e453d9ac
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-01-14 10:01:15 -0300

    Add request method POST to add new drink

--- exercises/22-intro-node-js/04-day-express-http/01-examples/02-para-fixar/index.js
+++ exercises/22-intro-node-js/04-day-express-http/01-examples/02-para-fixar/index.js
@@ -1,31 +1,38 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 const drinks = [
-  { id: 1, name: "Refrigerante Lata", price: 5.0 },
-  { id: 2, name: "Refrigerante 600ml", price: 8.0 },
-  { id: 3, name: "Suco 300ml", price: 4.0 },
-  { id: 4, name: "Suco 1l", price: 10.0 },
-  { id: 5, name: "Cerveja Lata", price: 4.5 },
-  { id: 6, name: "Água Mineral 500 ml", price: 5.0 },
+  { id: 1, name: 'Refrigerante Lata', price: 5.0 },
+  { id: 2, name: 'Refrigerante 600ml', price: 8.0 },
+  { id: 3, name: 'Suco 300ml', price: 4.0 },
+  { id: 4, name: 'Suco 1l', price: 10.0 },
+  { id: 5, name: 'Cerveja Lata', price: 4.5 },
+  { id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
 ];
 
-app.get("/drinks/search", (req, res) => {
+app.get('/drinks/search', (req, res) => {
   const { name } = req.query;
   console.log(req.query);
   const filteredDrinks = drinks.filter((d) => d.name.includes(name));
   res.status(200).json(filteredDrinks);
 });
 
-app.get("/drinks/:minPrice", (req, res) => {
+app.get('/drinks/:minPrice', (req, res) => {
   const { minPrice } = req.params;
   const drink = drinks.filter((d) => d.price >= minPrice);
 
-  if (!drink) return res.status(404).json({ message: "Drink not found!" });
+  if (!drink) return res.status(404).json({ message: 'Drink not found!' });
 
   res.status(200).json(drink);
 });
 
+app.post('/drinks', (req, res) => {
+  console.log(req.body);
+  const { id, name, price } = req.body;
+  drinks.push({ id, name, price });
+  res.status(201).json({ message: 'Drink created successfully.' });
+});
+
 app.listen(3001, () => {
-  console.log("Ouvindo na porta 3001!");
+  console.log('Ouvindo na porta 3001!');
 });
