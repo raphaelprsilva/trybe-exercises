e2b58c513a983d8cf131b9bfc3f2905124e92455
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-01-17 07:48:08 -0300

    Add middleware validation

---
+++ exercises/22-intro-node-js/04-day-express-http/01-examples/01-example/auth-middleware.js
@@ -0,0 +1,21 @@
+const validUser = {
+  username: 'prsRaphael',
+  password: '123456',
+};
+
+const authMiddleware = (req, res, next) => {
+  const { username, password } = req.headers;
+
+  if (!username && !password)
+    return res
+      .status(401)
+      .json({ message: 'Username or password can`t be blank.' });
+
+  if (username !== validUser.username || password !== validUser.password) {
+    return res.status(401).json({ message: 'Invalid crentials!' });
+  }
+
+  next();
+};
+
+module.exports = authMiddleware;
