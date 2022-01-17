f15ecbe61de24c70ab117f9103b3f6be22ad057b
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-01-17 07:33:48 -0300

    Add validatePrice function

--- exercises/22-intro-node-js/04-day-express-http/01-examples/01-example/index.js
+++ exercises/22-intro-node-js/04-day-express-http/01-examples/01-example/index.js
@@ -10,6 +10,14 @@ const recipes = [
   { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
 ];
 
+const validatePrice = (req, res, next) => {
+  const { price } = req.body;
+  if (typeof price === 'number' && price > 0)
+    return res.status(400).json({ message: 'Invalid price!' });
+
+  next();
+};
+
 app.get('/recipes/search', (req, res) => {
   const { name, maxPrice } = req.query;
   const filteredRecipes = recipes.filter(
@@ -34,7 +42,7 @@ app.get('/recipes', (_req, res) => {
   res.status(200).json(recipes);
 });
 
-app.post('/recipes', (req, res) => {
+app.post('/recipes', validatePrice, (req, res) => {
   const { id, name, price } = req.body;
   recipes.push({ id, name, price });
   res.status(201).json({ message: 'Recipe created successfully!' });
@@ -48,7 +56,7 @@ app.get('/validateToken', (req, res) => {
   res.status(200).json({ message: 'Valid Token!' });
 });
 
-app.put('/recipes/:id', (req, res) => {
+app.put('/recipes/:id', validatePrice, (req, res) => {
   console.log('req:', req);
   const { id } = req.params;
   const { name, price } = req.body;
