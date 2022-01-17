16fe5a9d11d14297c0713634cd5cad5391449b5a
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-01-17 05:52:09 -0300

    Add exercise 07

--- exercises/22-intro-node-js/04-day-express-http/02-exercises/02-exercise/index.js
+++ exercises/22-intro-node-js/04-day-express-http/02-exercises/02-exercise/index.js
@@ -9,9 +9,23 @@ app.use(bodyParser.json());
 
 app.get(
   '/simpsons',
-  rescue(async (req, res) => {
+  rescue(async (_req, res) => {
     const simpsons = await getSimpsons();
     res.status(200).json(simpsons);
+  })
+);
+
+app.get(
+  '/simpsons/:id',
+  rescue(async (req, res) => {
+    const { id } = req.params;
+    const simpsons = await getSimpsons();
+    const simpson = simpsons.find((s) => s.id === id);
+
+    if (!simpson)
+      return res.status(404).json({ message: `Simpson not found.` });
+
+    res.status(202).json(simpson);
   })
 );
 
