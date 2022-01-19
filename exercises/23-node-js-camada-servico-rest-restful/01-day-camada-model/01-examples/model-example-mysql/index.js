a8de246978c251b6133712ddc92048ab55eb129c
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-01-19 08:12:23 -0300

    Add route to insert new authors to data base

--- exercises/23-node-js-camada-servico-rest-restful/01-day-camada-model/01-examples/model-example-mysql/index.js
+++ exercises/23-node-js-camada-servico-rest-restful/01-day-camada-model/01-examples/model-example-mysql/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
 const Author = require('./models/Author');
 const Book = require('./models/Book');
+
+app.use(bodyParser.json());
 
 app.get('/authors', async (_req, res) => {
   const authors = await Author.getAll();
@@ -18,6 +21,18 @@ app.get('/authors/:id', async (req, res) => {
   if (!author) return res.status(404).json({ message: 'Not found author!' });
 
   res.status(200).json(author);
+});
+
+app.post('/authors', async (req, res) => {
+  const { first_name, middle_name, last_name } = req.body;
+
+  if (!Author.isValid(first_name, middle_name, last_name)) {
+    return res.status(400).json({ message: 'Invalid data' });
+  }
+
+  await Author.create(first_name, middle_name, last_name);
+
+  res.status(201).json({ message: 'Created author successfully!' })
 });
 
 app.get('/books', async (req, res) => {
