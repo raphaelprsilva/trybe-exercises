0f257d00ca1f5a25ba1d995c71090687c80180d3
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-01-19 07:53:31 -0300

    Add route to request author by id

--- exercises/23-node-js-camada-servico-rest-restful/01-day-camada-model/01-examples/model-example-mysql/index.js
+++ exercises/23-node-js-camada-servico-rest-restful/01-day-camada-model/01-examples/model-example-mysql/index.js
@@ -3,11 +3,27 @@ const app = express();
 const port = 3000;
 
 const Author = require('./models/Author');
+// const Book = require('./models/Book');
 
 app.get('/authors', async (_req, res) => {
   const authors = await Author.getAll();
 
   res.status(200).json(authors);
 });
+
+app.get('/authors/:id', async (req, res) => {
+  const { id } = req.params;
+  const author = await Author.findById(id);
+
+  if (!author) return res.status(404).json({ message: 'Not found author!' });
+
+  res.status(200).json(author);
+});
+
+// app.get('/books', async (req, res) => {
+//   const books = await Book.getAll();
+
+//   res.status(200).json(books);
+// })
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
