7c8a2dfc9b31fc305ff79dfacebd985009a1b3be
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-01-21 19:04:56 -0300

    Add GET route to /authors/:id that returns specific author by id

--- exercises/23-node-js-camada-servico-rest-restful/01-day-camada-model/01-examples/mode2-example-mysql/index.js
+++ exercises/23-node-js-camada-servico-rest-restful/01-day-camada-model/01-examples/mode2-example-mysql/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-const { getAllAuthors } = require('./models/Author');
+const { getAllAuthors, getAuthorsById } = require('./models/Author');
 const { getAllBooks, getBooksByAuthorId } = require('./models/Book');
 
 app.get('/authors',
@@ -18,6 +18,19 @@ app.get(
     const books = await getAllBooks();
     res.status(200).json(books);
 });
+
+app.get(
+  '/authors/:id',
+  async (req, res) => {
+    const { id } = req.params;
+    const authorById = await getAuthorsById(id);
+
+    if (!authorById) {
+      return res.status(404).json({ message: 'Author not found' });
+    }
+    res.status(200).json(authorById);
+  }
+)
 
 app.get(
   '/books/:id',
