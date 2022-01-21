73dcd37e0b96c2f211cfc8ee7ef560fd08dfeb59
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-01-21 17:53:10 -0300

    Add GET route to /books/:id end-point to return books by authorId

--- exercises/23-node-js-camada-servico-rest-restful/01-day-camada-model/01-examples/mode2-example-mysql/index.js
+++ exercises/23-node-js-camada-servico-rest-restful/01-day-camada-model/01-examples/mode2-example-mysql/index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 const { getAllAuthors } = require('./models/Author');
-const { getAllBooks } = require('./models/Book');
+const { getAllBooks, getBooksByAuthorId } = require('./models/Book');
 
 app.get('/authors',
   async (_req, res) => {
@@ -18,5 +18,14 @@ app.get(
     const books = await getAllBooks();
     res.status(200).json(books);
 });
+
+app.get(
+  '/books/:id',
+  async (req, res) => {
+    const { id } = req.params;
+    const booksByAuthorId = await getBooksByAuthorId(id);
+    res.status(200).json(booksByAuthorId);
+  }
+)
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
