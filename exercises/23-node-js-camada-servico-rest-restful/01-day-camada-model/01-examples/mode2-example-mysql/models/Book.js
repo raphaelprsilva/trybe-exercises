0b36bf9c959e5077114862c11533af03a381bc7c
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-01-22 12:12:02 -0300

    Add isValidBook to validate book data and add setNewBook to create new book at books table

--- exercises/23-node-js-camada-servico-rest-restful/01-day-camada-model/01-examples/mode2-example-mysql/models/Book.js
+++ exercises/23-node-js-camada-servico-rest-restful/01-day-camada-model/01-examples/mode2-example-mysql/models/Book.js
@@ -1,5 +1,5 @@
+const { getAuthorsById } = require('./Author');
 const connection = require('./connection');
-
 
 const serialize = (book) => ({
   id: book.id,
@@ -25,7 +25,27 @@ const getBooksByAuthorId = async (bookId) => {
   return { id, title, authorId };
 };
 
+const isAValidBook = async (title, authorId) => {
+  console.log('title:', title);
+  console.log('typeof title:', typeof title);
+  console.log('title.length:', title.length);
+  console.log('authorId:', authorId);
+  console.log('typeof authorId:', typeof authorId);
+  console.log('!(await getAuthorsById(authorId):', !(await getAuthorsById(authorId)));
+  if (!title || typeof title !== 'string' || title.length < 3) return false;
+  if (!authorId || typeof authorId !== 'number' || !(await getAuthorsById(authorId))) return false;
+  return true;
+};
+
+const setNewBook = async (title, authorId) => {
+  const QUERY =
+    'INSERT INTO model_example.books (title, author_id) VALUES (?, ?)';
+  connection.execute(QUERY, [title, authorId]);
+};
+
 module.exports = {
   getAllBooks,
   getBooksByAuthorId,
+  isAValidBook,
+  setNewBook,
 };
