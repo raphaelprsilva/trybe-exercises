9b324bca452761ca34c958b744f8a520045a5000
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-01-22 14:48:53 -0300

    \Add User.js

---
+++ exercises/23-node-js-camada-servico-rest-restful/01-day-camada-model/02-exercises/01-exercise/models/User.js
@@ -0,0 +1,41 @@
+const connection = require('./connection');
+
+// Properties validators functions
+const validatePropertyAndType = (property) => !property || typeof property !== 'string';
+
+const serialize = ({ id, first_name, last_name, email, password }) => ({
+  id,
+  firstName: first_name,
+  lastName: last_name,
+  email,
+  password,
+});
+
+const getAllUsers = async () => {
+  const QUERY = 'SELECT id, first_name, last_name, email, password FROM users_crud.users';
+  const [users] = await connection.execute(QUERY);
+  return users.map(serialize);
+};
+
+const validateNewUser = ({ firstName, lastName, email, password }) => {
+  if (validatePropertyAndType(firstName)) return false;
+  if (validatePropertyAndType(lastName)) return false;
+  if (validatePropertyAndType(email)) return false;
+  if (validatePropertyAndType(password) || password.length < 6) return false;
+  return true;
+};
+
+const setNewUser = async ({ firstName, lastName, email, password }) => {
+  const QUERY =
+    'INSERT INTO users_crud.users (first_name, last_name, email, password) VALUES(?, ?, ?, ?)';
+  // connection.execute(QUERY, [firstName, lastName, email, password])
+  console.log(
+    connection.execute(QUERY, [firstName, lastName, email, password])
+  );
+};
+
+module.exports = {
+  getAllUsers,
+  setNewUser,
+  validateNewUser,
+};
