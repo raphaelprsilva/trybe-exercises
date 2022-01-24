0630ffb7e91ce350c4a65f6644ea0da86a72ec63
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-01-24 18:50:24 -0300

    Remove getNewAuthor function reference from dependent functions

--- exercises/23-node-js-camada-servico-rest-restful/02-day-camada-controller-service/01-examples/hello-msc/models/Author.js
+++ exercises/23-node-js-camada-servico-rest-restful/02-day-camada-controller-service/01-examples/hello-msc/models/Author.js
@@ -1,14 +1,15 @@
 const connection = require('./connection');
 
 // Converte o nome dos campos de snake_case para camelCase
-const serialize = (authorData) =>
-  authorData.map((item) =>
-    getNewAuthor({
-      id: item.id,
-      firstName: item.first_name,
-      middleName: item.middle_name,
-      lastName: item.last_name,
-    })
+const serialize = (authorData) => authorData
+  .map((item) => {
+      return {
+        id: item.id,
+        firstName: item.first_name,
+        middleName: item.middle_name,
+        lastName: item.last_name,
+      }
+    }
   );
 
 // Busca todos os autores do banco.
@@ -40,7 +41,7 @@ const createAuthor = async (firstName, middleName, lastName) => {
     'INSERT INTO model_example.authors (first_name, middle_name, last_name) VALUES (?, ?, ?)',
     [firstName, middleName, lastName]
   );
-  return getNewAuthor({ id: author.insertId, firstName, middleName, lastName });
+  return { id: author.insertId, firstName, middleName, lastName };
 };
 
 module.exports = {
