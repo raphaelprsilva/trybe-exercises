fbf49ebdc6236d4b78771296a47bf160939979e7
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-01-24 06:35:51 -0300

    Add user and error middlewares

--- exercises/23-node-js-camada-servico-rest-restful/01-day-camada-model/02-exercises/01-exercise/index.js
+++ exercises/23-node-js-camada-servico-rest-restful/01-day-camada-model/02-exercises/01-exercise/index.js
@@ -2,12 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-const User = require('./models/User');
+const middlewares = require('./middlewares/index');
 
 app.use(bodyParser.json());
-
-const responseJSONMessage = (res, status, message) => res
-  .status(status).json({ error: true, message });
 
 app.get(
   '/users',
@@ -17,22 +14,10 @@ app.get(
   res.status(200).json(users);
 });
 
-app.post(
-  '/user',
-  async (req, res) => {
-    const { first_name, last_name, email, password } = req.body;
-    const newUser = User
-      .validateNewUser({ first_name, last_name, email, password });
-    const errorMessage = 'Can not create new user';
-    console.log('newUser:', newUser);
+app.post('/user', middlewares.createUser);
 
-    if (!newUser) return responseJSONMessage(res, 400, errorMessage);
+app.use(middlewares.error);
 
-    await User.setNewUser(first_name, last_name, email, password);
-    res.status(201).json(newUser);
-  }
-);
-
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
