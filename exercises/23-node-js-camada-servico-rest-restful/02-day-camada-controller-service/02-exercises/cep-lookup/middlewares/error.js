b46ac1241011c938f5e8968728d45122651422f1
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-01-26 10:11:32 -0300

    Add error.js middleware

---
+++ exercises/23-node-js-camada-servico-rest-restful/02-day-camada-controller-service/02-exercises/cep-lookup/middlewares/error.js
@@ -0,0 +1,22 @@
+module.exports = (err, _req, res, _next) => {
+  if (err.isJoi) {
+    return res.status(400).json({ error: { message: err.details[0].message } });
+  }
+
+  if (err.code) {
+    const statusByErrorCode = {
+      notFound: 404,
+    };
+
+    const status = statusByErrorCode[err.code] || 500;
+
+    res.status(status).json(err);
+  }
+
+  console.log(err);
+  res
+    .status(500)
+    .json({ error: { code: 'internal', message: 'Internal server error' } });
+};
+
+
