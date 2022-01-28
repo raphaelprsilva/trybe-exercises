2793581374f2a2fe13ee39334ca0ba7df9750a47
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-01-28 08:50:01 -0300

    Add middleware to validate new adrres creation function

--- exercises/23-node-js-camada-servico-rest-restful/02-day-camada-controller-service/02-exercises/cep-lookup/controllers/Cep.js
+++ exercises/23-node-js-camada-servico-rest-restful/02-day-camada-controller-service/02-exercises/cep-lookup/controllers/Cep.js
@@ -1,5 +1,6 @@
 const rescue = require('express-rescue');
 const CepService = require('../services/Cep');
+const Joi = require('joi');
 
 const findAddressByCep = rescue(async (req, res, next) => {
   const { cep } = req.params;
@@ -10,6 +11,27 @@ const findAddressByCep = rescue(async (req, res, next) => {
   res.status(200).json(address);
 });
 
+const createNewAddress = rescue(async (req, res, next) => {
+  const requiredNonEmptyString = Joi.string().not().empty().required();
+  
+  const { error } = Joi.object({
+    cep: Joi.string().regex(/\d{5}-\d{3}/).required(),
+    logradouro: requiredNonEmptyString,
+    bairro: requiredNonEmptyString,
+    localidade: requiredNonEmptyString,
+    uf: requiredNonEmptyString.length(2),
+  }).validate(req.body);
+
+  if (error) return next(error);
+
+  const newCep = await CepService.createNewAddress(req.body);
+
+  if (newCep.error) return next(newCep.error);
+
+  res.status(200).json(newCep);
+})
+
 module.exports = {
   findAddressByCep,
+  createNewAddress,
 };
