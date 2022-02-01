d0038a8a158753ce03666145fd74952aefe2d5a7
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-02-01 18:37:56 -0300

    Move config, controllers, migrations and seeders to src directory

--- exercises/24-node-js-orm-autenticacao/01-day-interface-app-db/01-examples/app-with-sequelize/index.js
+++ exercises/24-node-js-orm-autenticacao/01-day-interface-app-db/01-examples/app-with-sequelize/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const userController = require('./controllers/userController');
+const userController = require('./src/controllers/userController');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
