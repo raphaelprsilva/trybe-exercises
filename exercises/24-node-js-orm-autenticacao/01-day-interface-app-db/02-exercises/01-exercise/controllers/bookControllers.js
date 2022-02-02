8e790542f8d936d5f83f2b5939f236043aa715b6
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-02-02 17:06:07 -0300

    Add post route to create new book

--- exercises/24-node-js-orm-autenticacao/01-day-interface-app-db/02-exercises/01-exercise/controllers/bookControllers.js
+++ exercises/24-node-js-orm-autenticacao/01-day-interface-app-db/02-exercises/01-exercise/controllers/bookControllers.js
@@ -13,4 +13,16 @@ router.get('/', async (_req, res) => {
   }
 });
 
+router.post('/', async (req, res) => {
+  try {
+    const { title, author, pageQuantity, createdAt } = req.body;
+    const newBook = await Book.create({ title, author, pageQuantity, createdAt });
+
+    res.status(200).json(newBook);
+  } catch (error) {
+    console.log(`${error.name}: ${error.message}`);
+    res.status(500).json({ message: 'Algo deu errado ❌' });
+  }
+});
+
 module.exports = router;
