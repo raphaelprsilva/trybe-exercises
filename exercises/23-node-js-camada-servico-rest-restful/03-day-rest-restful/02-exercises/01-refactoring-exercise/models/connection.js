26c074485c1a9c4f53e712f8d60c8f37f8cda01c
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-02-04 18:13:03 -0300

    Change database configuration

--- exercises/23-node-js-camada-servico-rest-restful/03-day-rest-restful/02-exercises/01-refactoring-exercise/models/connection.js
+++ exercises/23-node-js-camada-servico-rest-restful/03-day-rest-restful/02-exercises/01-refactoring-exercise/models/connection.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: 'localhost',
   user: 'raphael',
   password: 'Raphael123456!',
-  database: 'b24_d03_exercises'
+  database: 'b24_d03_exercises',
 });
 
 module.exports = connection;
