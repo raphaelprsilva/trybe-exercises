f17525ae434dd7d9b521536155a4f8050039620a
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-02-08 10:03:26 -0300

    Add exercise 02

--- exercises/22-intro-node-js/02-day-async-flow/02-exercises/00-content-review/01-exercise/index.js
+++ exercises/22-intro-node-js/02-day-async-flow/02-exercises/00-content-review/01-exercise/index.js
@@ -1,6 +1,12 @@
 const areParamsNums = (paramsArray) => paramsArray
   .every((num) => typeof num === 'number');
 
+const getRandomNumber = () => Math.floor(Math.random() * 100 + 1);
+
+const randomNumber1 = getRandomNumber();
+const randomNumber2 = getRandomNumber();
+const randomNumber3 = getRandomNumber();
+
 const doCalc = (param1, param2, param3) => {
   return new Promise((resolve, reject) => {
     const areAllNumbers = areParamsNums([param1, param2, param3]);
@@ -12,6 +18,6 @@ const doCalc = (param1, param2, param3) => {
   });
 };
 
-doCalc(1, 2, 50)
+doCalc(randomNumber1, randomNumber2, randomNumber3)
   .then((resolve) => console.log(resolve))
   .catch((error) => console.error(error));
