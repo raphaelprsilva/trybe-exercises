0fe52d7fe3795312bafdd8db27d965d1f6257b6d
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-02-08 09:10:25 -0300

    Using promises to read file asynchronously

--- exercises/22-intro-node-js/02-day-async-flow/01-examples/00-content-review/io-local/readFile.js
+++ exercises/22-intro-node-js/02-day-async-flow/01-examples/00-content-review/io-local/readFile.js
@@ -1,11 +1,20 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 
-const fileName = 'meu-arquivo2.txt';
+const fileName = 'meu-arquivo.txt';
 
-fs.readFile(fileName, 'utf8', (err, data) => {
-  if (err) {
+// Antes => Usando callback => Não recomendado
+// fs.readFile(fileName, 'utf8', (err, data) => {
+//   if (err) {
+//     console.error(`Não foi possível ler o arquivo ${fileName}.\nErro: ${err}`);
+//     process.exit(1);
+//   }
+//   console.log(`Conteúdo do arquivo: ${data}`);
+// });
+
+// Depois => Utilizando Promises => Mais Recomendado
+fs.readFile(fileName, 'utf8')
+  .then((data) => console.log(`Conteúdo do arquivo: ${data}`))
+  .catch((err) => {
     console.error(`Não foi possível ler o arquivo ${fileName}.\nErro: ${err}`);
     process.exit(1);
-  }
-  console.log(`Conteúdo do arquivo: ${data}`);
-})
+  });
