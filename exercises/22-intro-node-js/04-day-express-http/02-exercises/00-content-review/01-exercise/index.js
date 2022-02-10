ce279541a28ef5f4a4069b280ccd3fd7db7357af
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-02-10 15:49:51 -0300

    Add POST route to /hello and change app.all to last position

--- exercises/22-intro-node-js/04-day-express-http/02-exercises/00-content-review/01-exercise/index.js
+++ exercises/22-intro-node-js/04-day-express-http/02-exercises/00-content-review/01-exercise/index.js
@@ -5,13 +5,18 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.all('*', (req, res) => {
-  res.status(404).json({ message: `Rota '${req.path}' não existe.`});
-});
-
 // Exercício 01
 app.get('/ping', (req, res) => {
   res.status(200).json({ message: 'pong' });
+});
+
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  res.status(201).json({ message: `Hello ${name}!`});
+});
+
+app.all('*', (req, res) => {
+  res.status(404).json({ message: `Rota '${req.path}' não existe.` });
 });
 
 app.listen(3000, () => console.log('Listening on port 3000.'));
