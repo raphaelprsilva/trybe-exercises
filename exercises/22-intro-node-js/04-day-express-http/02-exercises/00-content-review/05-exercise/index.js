b4ff19c771ff57478c32cf8fc9a46b606c2d9ae6
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-02-10 17:21:05 -0300

    Add exercise 07

--- exercises/22-intro-node-js/04-day-express-http/02-exercises/00-content-review/05-exercise/index.js
+++ exercises/22-intro-node-js/04-day-express-http/02-exercises/00-content-review/05-exercise/index.js
@@ -6,7 +6,7 @@ const chalk = require('chalk');
 
 // Função para logar os testes
 const logBlue = (messageToLog, variable) =>
-  console.log(chalk.blue.bold(messageToLog), variable);
+  console.log(chalk.blue.bold.inverse(messageToLog), variable);
 
 const utils = require('./fs-utils');
 
@@ -22,6 +22,21 @@ app.get('/simpsons', rescue(async (_req, res) => {
   }
 
   res.status(200).json(simpsons);
+}));
+
+app.get('/simpsons/:id', rescue(async (req, res) => {
+  const { id } = req.params;
+  const simpsons = await utils.getSimpsons();
+  logBlue('simpsons:', simpsons);
+  const simpsonsFilteredById = simpsons.filter(
+    (simpson) => simpson.id === id
+  );
+
+  if (simpsonsFilteredById.length === 0) {
+    return res.status(404).json({ message: 'Simpsons not found.' });
+  }
+
+  res.status(200).json(simpsonsFilteredById);
 }));
 
 app.all('*', (req, res) => {
