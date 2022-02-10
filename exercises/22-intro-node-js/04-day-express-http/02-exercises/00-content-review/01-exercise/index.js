dadd2c32e572569816bfcd1f0b58422d5cbad6c2
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-02-10 15:46:00 -0300

    Add ALL method to capture non existent routes

--- exercises/22-intro-node-js/04-day-express-http/02-exercises/00-content-review/01-exercise/index.js
+++ exercises/22-intro-node-js/04-day-express-http/02-exercises/00-content-review/01-exercise/index.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.all('*', (req, res) => {
+  res.status(404).json({ message: `Rota '${req.path}' não existe.`});
+});
+
 // Exercício 01
 app.get('/ping', (req, res) => {
   res.status(200).json({ message: 'pong' });
