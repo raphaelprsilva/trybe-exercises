bc35a74888957aed5026deb95b4c4dc94e802602
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-03-01 18:19:12 -0300

    add transactions

--- exercises/24-node-js-orm-autenticacao/02-day-associations/01-examples/00-content-review/01-example/controllers/employeeControllers.js
+++ exercises/24-node-js-orm-autenticacao/02-day-associations/01-examples/00-content-review/01-example/controllers/employeeControllers.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const Sequelize = require('sequelize');
 
 const { Employee, Address } = require('../models');
+const config = require('../config/config');
+
+const sequelize = new Sequelize(config.development);
 
 router.get('/', async (_req, res) => {
   try {
@@ -49,6 +53,36 @@ router.get('/:id', async (req, res) => {
 
     return res.status(200).json(employee);
   } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
+});
+
+router.post('/employees', async (req, res) => {
+  const t = await sequelize.transaction();
+
+  try {
+    const { firstName, lastName, age, city, street, number } = req.body;
+
+    const employee = await Employee.create(
+      { firstName, lastName, age },
+      { transaction: t }
+    );
+
+    await Address.create(
+      { city, street, number, employeeId: employee.id },
+      { transaction: t }
+    );
+
+    // Se chegou até essa linha, quer dizer que nenhum erro ocorreu.
+    // Com isso, podemos finalizar a transação usando a função `commit`.
+    await t.commit();
+
+    return res.status(201).json({ message: 'Cadastrado com sucesso' });
+  } catch (e) {
+    // Se entrou nesse bloco é porque alguma operação falhou.
+    // Nesse caso, o sequelize irá reverter as operações anteriores com a função rollback, não sendo necessário fazer manualmente
+    await t.rollback();
     console.log(e.message);
     res.status(500).json({ message: 'Algo deu errado' });
   }
