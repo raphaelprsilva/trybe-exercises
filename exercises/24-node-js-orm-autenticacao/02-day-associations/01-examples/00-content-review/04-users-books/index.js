eec697524e55447414f212c9d68813b8d04b1999
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-03-02 16:11:59 -0300

    add example

---
+++ exercises/24-node-js-orm-autenticacao/02-day-associations/01-examples/00-content-review/04-users-books/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const userController = require('./controllers/userController');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use('/userbooks', userController);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
---
+++ exercises/24-node-js-orm-autenticacao/02-day-associations/01-examples/00-content-review/04-users-books/migrations/20220302174621-create-book.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Books', {
+      bookId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        field: 'book_id',
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      releaseYear: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        field: 'release_year',
+      },
+      numberPages: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        field: 'number_pages',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'created_at',
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'updated_at',
+      },
+    });
+  },
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('Books');
+  },
+};
